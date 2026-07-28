81b50dbb4abb5c2b35cb538ca1eeed9158cd53c6
Nathan Bachetti <nathanbachetti@gmail.com>
2026-07-28 22:11:25 +1000

    new psexp class

--- src/init.c
+++ src/init.c
@@ -20,6 +20,7 @@ extern SEXP symlinkR        (SEXP, SEXP, SEXP, SEXP);
 extern SEXP oopr_cont_init  (SEXP, SEXP);
 extern SEXP find_instances  (SEXP, SEXP, SEXP);
 extern SEXP eval_context    (SEXP, SEXP, SEXP);
+extern SEXP get_PSEXPs      ( );
 
 static const R_CallMethodDef CallEntries[] = {
     {"find_member_refs", (DL_FUNC) &find_member_refs, 1},
@@ -34,12 +35,14 @@ static const R_CallMethodDef CallEntries[] = {
     {"oopr_cont_init",   (DL_FUNC) &oopr_cont_init,   3},
     {"find_instances",   (DL_FUNC) &find_instances,   3},
     {"eval_context",     (DL_FUNC) &eval_context,     3},
+    {"get_PSEXPs",       (DL_FUNC) &get_PSEXPs,       0},
     {NULL, NULL, 0}
 };
 
-void R_init_oopr(DllInfo *dll)
+void R_init_oopr(DllInfo* dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
     R_forceSymbols(dll, TRUE);
 }
+
---
+++ src/util/psexp.cpp
@@ -0,0 +1,131 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+#include "./util/psexp.h"
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Constructor, destructor
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+PSEXP::PSEXP(SEXP x) : tkn(store(x)) { }
+PSEXP::~PSEXP( ) noexcept { release(tkn); }
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Can be moved. The object moved from's token then becomes NULL so
+ * release() will be skipped on destruction.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+PSEXP::PSEXP(PSEXP&& x) noexcept : tkn(x.tkn) { x.tkn = R_NilValue; }
+PSEXP& PSEXP::operator=(PSEXP&& x) noexcept
+{
+  if(this != &x)
+  {
+    release(tkn);
+    tkn = x.tkn;
+    x.tkn = R_NilValue;
+  }
+  return *this;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * If not token not initialized, just return NULL
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+SEXP PSEXP::get( ) const noexcept { return empty() ? R_NilValue : TAG(tkn); }
+PSEXP::operator SEXP( ) const noexcept { return get(); }
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Set the underlying SEXP
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+void PSEXP::set(SEXP x)
+{
+  if(empty())
+  {
+    tkn = store(x);
+  }
+  else
+  {
+    SET_TAG(tkn, x);
+  }
+}
+PSEXP& PSEXP::operator=(SEXP x)
+{
+  if(x != get()) set(x);
+  return *this;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Check if the token has been initialized
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+bool PSEXP::empty() const noexcept { return tkn == R_NilValue; }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Static stuff.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Lazily create a pairlist object and protect it, only happens once.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+SEXP PSEXP::init() noexcept
+{
+  SEXP out = Rf_cons(R_NilValue, Rf_cons(R_NilValue, R_NilValue));
+  R_PreserveObject(out);
+  return out;
+}
+SEXP PSEXP::root() noexcept
+{
+  static SEXP out = init(); // ensure only one protected pairlist exists
+  return out;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Store a SEXP in the protected pairlist object.
+ * Inserts a new pairlist object at the top of the protected pairlist, which
+ *   - Holds a pointer to the pairlist prior,
+ *   - Holds a pointer to the parilist after, and
+ *   - Sets the SEXP object as the TAG.
+ * Returns the new pairlist, where the SEXP object can be returned using
+ * the TAG function.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+SEXP PSEXP::store(SEXP x) noexcept
+{
+  if(x == R_NilValue) return x;
+  PROTECT(x);
+  SEXP head  = root();
+  SEXP next  = CDR(head);
+  SEXP token = PROTECT(Rf_cons(head, next));
+  SET_TAG(token, x);
+  SETCDR(head, token);
+  SETCAR(next, token);
+  UNPROTECT(2);
+  return token;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+ * Remove the token from the protected pairlist.
+ * Because the token holds pointers to the pairlists prior and after, those
+ * can be amended to remove the pointer to the token.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+void PSEXP::release(SEXP token) noexcept
+{
+  if(token == R_NilValue) return; // uninitialized token is skipped
+  SEXP lhs = CAR(token);
+  SEXP rhs = CDR(token);
+  SETCDR(lhs, rhs);
+  SETCAR(rhs, lhs);
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+R_xlen_t PSEXP::size( ) noexcept { return Rf_xlength(root()) - 2; }
+SEXP     PSEXP::list( ) noexcept
+{
+  SEXP out  = PROTECT(R_NilValue);
+  for(SEXP x = CDR(root()); CDR(x) != R_NilValue; x = CDR(x))
+  {
+    out = Rf_cons(TAG(x), out);
+  }
+  UNPROTECT(1);
+  return out;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+SEXP get_PSEXPs()
+{
+  return PSEXP::list();
+}
